c65f7cdd700efcf3e96c2bb9e62b5891f9aa0423
remy.d1 <remy.d1@gmail.com>
2016-01-13 16:29:47 +0100

    dhcp rm script update

--- VirtualBox/remove-vbox-dhcp-servers.sh
+++ VirtualBox/remove-vbox-dhcp-servers.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 echo "This script will remove every DHCP servers in your Virtualbox. Are you sure ? [Yy/Nn]"
+read -n 1 rmdhcp
 
 # assuming awk and vboxmanage are both in the PATH
 
 DHCPservers=`vboxmanage list dhcpservers | awk -F"[: ]" '/NetworkName/ {print $2}'`
 
-
-for server in $DHCPservers
-    do
-    vboxmanage dhcpserver remove --netname $server
-done
+if [ "$rmdhcp" == "y" ] || [ "$rmdhcp" == "Y" ]; then
+    for server in $DHCPservers
+        do
+        vboxmanage dhcpserver remove --netname $server
+    done
+fi
